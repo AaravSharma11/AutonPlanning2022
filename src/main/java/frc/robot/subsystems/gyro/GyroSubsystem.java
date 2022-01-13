438d6a87daacf43bf178a54c1fb5590999877f55
pryme-svg <edoc.www@gmail.com>
2022-01-12 18:29:34 -0800

    use faster spi interface

--- src/main/java/frc/robot/subsystems/gyro/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/gyro/GyroSubsystem.java
@@ -1,10 +1,11 @@
 package frc.robot.subsystems.gyro;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.kauailabs.navx.frc.AHRS;
 
 public class GyroSubsystem extends SubsystemBase {
 
@@ -15,9 +16,9 @@ public class GyroSubsystem extends SubsystemBase {
         SmartDashboard.putString("subsystem", "init gyro sub");
         try {
 			/* Communicate w/navX-MXP via the MXP SPI Bus (use mini USB to USB A cable)   
-			   Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or S     
+			   Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SPI.Port.kMXP   
 			   See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-               ahrs = new AHRS(I2C.Port.kMXP); 
+               ahrs = new AHRS(SPI.Port.kMXP); 
 		} catch (RuntimeException ex ) {
             DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true); 
         }
